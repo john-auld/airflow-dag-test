612fca9ceb4698f9f29565e2d9272177afac8dbd
john-auld <43154541+john-auld@users.noreply.github.com>
2020-02-02 00:29:51 +0000

    fix with DAG

--- s3_list_bucket.py
+++ s3_list_bucket.py
@@ -2,7 +2,9 @@ from airflow.models import DAG
 from airflow.contrib.operators.s3_list_operator import S3ListOperator
 from datetime import datetime
 
-with DAG(dag_id='s3_list_bucket') as dag:
+with DAG(dag_id='s3_list_bucket',
+         schedule_interval=None,
+         start_date=datetime(2019, 6, 7)) as dag:
 
     s3_file = S3ListOperator(
         task_id='list_3s_files',
